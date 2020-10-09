dd6bff0565343f3bc1bb0168df50de448f47d72c
Yanina Trekhleb <yanisavchenko@gmail.com>
2020-10-09 17:15:37 +0300

    Ability to add new todo.

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Main Title in APP</h1>
+    <ToDoList />
+  </div>
+</template>
+
+<script>
+import ToDoList from './components/ToDoList'
+
+export default {
+  name: 'App',
+  components: {
+    ToDoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  margin: 0 auto;
+  width: 60%;
+}
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="todo-list">
+    <h2>To Do List Tasks</h2>
+    <input 
+      type="text" 
+      class="todo-input" 
+      placeholder="What needs to be done"
+      v-model="newTodo"
+      @keyup.enter="addNewToDo"
+    >
+    <div 
+      v-for="todo in todos" 
+      :key="todo.id" 
+      class="todo-item"
+    >
+      {{ todo.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'ToDoList',
+  data () {
+    return {
+      newTodo: '',
+      idForTodo: 3,
+      todos: [
+        {
+          id: 1,
+          title: 'Make course ready',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Water Flowers',
+          completed: false
+        },
+      ]
+    }
+  },
+  methods: {
+    addNewToDo() {
+      if (this.newTodo.trim().length == 0) {
+        return
+      }
+
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false
+      })
+
+      this.newTodo = ''
+      this.idForTodo++
+    }
+  }
+}
+</script>
+
+<style>
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 1.2em;
+  }
+</style>
